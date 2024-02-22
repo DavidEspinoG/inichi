29afff74728dcb5245968c77fb503f5641764560
David Espino <davidespinog@gmail.com>
2024-02-21 18:33:59 -0600

    Add content to about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,14 @@
 const About = () => {
 
   return (
-    <>
-      Nosotros
-    </>
+    <div className="bg-sand text-center p-7 min-h-[400px]">
+      <h1 className="text-olive uppercase text-2xl mb-5">Nosotros</h1>
+      <p className="text-olive capitalize">Somos un equipo constituido por mentes en desarrollo profesional con ideas innovadoras y frescas que satisfacen sus necesidades tecnológicas.</p>
+      <br/>
+      <p className="text-olive font-thin">Gracias a la destreza en el desarrollo de sistemas del equipo de colaboradores, la excelencia y certeza de nuestros productos nos han destacado como una de las empresas clave en servicios de TI, headhunting, fabricación de software, ciberseguridad, servicios de la nube y staffing especializado en perfiles de TI para varios de nuestros aliados estratégicos. </p>
+      <br/>
+      <p className="text-olive font-thin">Somos una empresa 100% mexicana que cuenta con un extenso grupo de especialistas que gracias a su capacidad y experiencia en todos los servicios de TI, brindan las mejores prácticas de la industria y garantizan la mejor solución para asegurar la operación, calidad y eficacia de los procesos cruciales de su negocio, contribuyendo al propósito de lograr una rentabilidad superior del mismo. </p>
+    </div>
   )
 };
 

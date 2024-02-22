ec6632af8782a9dc3a7d921a4430d106bf762e55
David Espino <davidespinog@gmail.com>
2024-02-21 18:19:45 -0600

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
         <Menu /> 
       </div>
       <Outlet />
-      {/* <Footer />  */}
+      <Footer /> 
     </div>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
 
     return (
-      <div>
-        Footer
+      <div className="bg-sand flex flex-col items-center justify-evenly gap-5 p-3">
+        <Link to='/' className="text-olive uppercase text-lg">Inicio</Link>
+        <Link to='about'  className="text-olive uppercase text-lg">Nosotros</Link>
+        <Link to='services' className="text-olive uppercase text-lg">Servicios</Link>
+        <Link to='contact' className="text-olive uppercase text-lg">Contáctanos</Link>
       </div>
     )
 };
